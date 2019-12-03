49df1aebccc1950005715d132713fcd3406708e9
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-03 23:15:38 +0200

    Updates

---
+++ precache-manifest.e88093cbaf81281a6b68a6c0160a37d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653582cc1fe89a218fe08f8f3cd4d0f6",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "ab511d1b55eba09d3012",
+    "url": "/react_static-list-of-todos/static/css/main.adfcd2b8.chunk.css"
+  },
+  {
+    "revision": "0299494b91f0aa0e5b2b",
+    "url": "/react_static-list-of-todos/static/js/2.c95df19c.chunk.js"
+  },
+  {
+    "revision": "ab511d1b55eba09d3012",
+    "url": "/react_static-list-of-todos/static/js/main.0d6770bf.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.e88093cbaf81281a6b68a6c0160a37d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_static-list-of-todos/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
